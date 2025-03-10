4170233624117863bab496ceab679c2370664c4e
Alicia-2007 <aligabmarfredocs@gmail.com>
2025-03-10 11:13:28 -0400

    clase 1
    ejercicios y pruebas

---
+++ clase 1.py
@@ -0,0 +1,40 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+#práctica
+a= 5
+b= 3
+resultado= a+b
+print(resultado) 
+
+saludo ="Hola,"
+nombre= "Pedro"
+mensaje= saludo + nombre
+print(mensaje)
+
+edad= 17
+es_adulto = edad > 18
+print(es_adulto)
+
+#ejercicios
+saludo = "Hola,"
+nombre = "me llamo Alicia,"
+edad = "tengo 17 años. "
+ciudad= "Soy de Ciudad Ojeda, pero estoy residenciada en Mcbo actualmente."
+mensaje= saludo + nombre+ edad+ ciudad
+print (f"presentación:",mensaje)
+
+estudiante= "Carlos"
+castellano= 10
+matematica= 20
+fisica= 10
+total_de_puntos = castellano + matematica + fisica
+promedio = total_de_puntos / 3
+print (f"El promedio de",estudiante, "es de", promedio)
+
+#curiosidad 
+nombre = input('ingresa nombre: ')
+edad = int(input('ingresa edad: '))
+ciudad = input('ingresa ciudad: ')
+
+print(f' Mi nombre es {nombre} y tengo {edad} años, vivo en {ciudad}')
+

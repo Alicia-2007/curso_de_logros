632c232b023795f11bba488e21531dd7a5ffe801
Alicia-2007 <aligabmarfredocs@gmail.com>
2025-03-19 21:55:44 -0400

    ejercicios 2da clase

---
+++ modulo_2/clase 2 ejemplos.js
@@ -0,0 +1,32 @@
+//info
+function unaFuncion(){
+    console.log(1)
+    console.log(2)
+    console.log(3)
+}
+unaFuncion();
+unaFuncion();
+unaFuncion();
+
+function unaFuncion(){
+    console.log(1);
+    return "hola";
+
+}
+const saludo= unaFuncion();
+console.log(saludo);
+
+function suma(a,b){
+    return a+b;
+}
+const resultado= (5,10);
+console.log(resultado);
+
+function suma(a,b){
+    return a+b;
+}
+const unaFuncion= function() =>{
+    console.log(1);
+};
+
+unaFuncion();
---
+++ modulo_2/clase 2 tarea 1.js
@@ -0,0 +1,20 @@
+
+//calculadora de edad, programa que pida el año de nacimiento y calcule la edad actual del usuario
+let edad;{
+      edad=parseInt (prompt("Introduce tu año de nacimiento:"));
+      año_actual=parseInt (prompt("Introduce el año actual:"));
+      const calculo= (edad, año_actual)=> año_actual-edad
+      console.log ("calculo:", calculo (edad, año_actual))
+  }
+
+//suma de numeros pares escribe un bucle que sume todos los numeros pares del 1 al 100
+for (let i==0;i<100;i/2==0){
+    console.log (i);
+}
+
+//tabla de multiplicar, pide al usuario un numero, y muestre la tabla de multiplicar de ese numero del 1 al 10
+let num;{
+      num=parseInt (prompt("Introduce un numero:"));
+      let multiplos [1,2,3,4,5,6,7,8,9,10].map (num =>num*multiplos);
+}
+console.log (num)

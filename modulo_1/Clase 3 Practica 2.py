b01c1a110956bdcbc28e4ed5f878e2ee02783496
Alicia-2007 <aligabmarfredocs@gmail.com>
2025-03-19 21:44:19 -0400

    Add files via upload

---
+++ modulo_1/Clase 3 Practica 2.py
@@ -0,0 +1,17 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+#Clase 3 Practica 2
+#diccionario
+diccionario= {"nombre": "Alicia",
+"apellido": "Marcano", 
+"edad": "17", 
+"pelicula_favorita":"Dr.Sleep"}
+print(diccionario)
+#diccionario interactivo cs im bored
+diccionario= {
+"nombre": input("Ingrese su nombre:"), 
+"apellido":input("Ingrese su apellido:") , 
+"edad":int(input("Ingrese su edad:")),
+"pelicula_favorita":input("Ingrese su pelicula preferida:")}
+print(diccionario)
+

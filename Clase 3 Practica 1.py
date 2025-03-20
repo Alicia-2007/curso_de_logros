4fa8db138321614419a72a260e2f3b7aa14e9a2f
Alicia-2007 <aligabmarfredocs@gmail.com>
2025-03-19 21:50:34 -0400

    Delete Clase 3 Practica 1.py

--- Clase 3 Practica 1.py
+++
@@ -1,9 +0,0 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-#Clase 3 Practica 1
-#lista
-alumnos= ["Jose Muñoz", "Jose Aular","Jose Nava","Juan David", "Genesis Echeto", "Raquel Valbuena", "Paula Valbuena", "Alberto Vidal","Hebert Rodriguez","Alicia Marcano", "Gabriel Henriquez", "Santiago Mesa", "Adan Moreno","Angel Medina"]
-print(f"los alumnos son {alumnos}")
-alumnos[10]="Daniel Espitia"
-print(f"los nuevos alumnos son {alumnos}")
-
